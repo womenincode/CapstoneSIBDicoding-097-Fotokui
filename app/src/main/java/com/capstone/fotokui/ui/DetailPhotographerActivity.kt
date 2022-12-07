8abdce564d435d3fd67f3c3516c49857d12fb8ed
rendiputra <rendiputrapradana@gmail.com>
2022-12-07 16:31:31 +0700

    Add DetailPhotographer features

---
+++ app/src/main/java/com/capstone/fotokui/ui/DetailPhotographerActivity.kt
@@ -0,0 +1,13 @@
+package com.capstone.fotokui.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.fotokui.R
+
+class DetailPhotographerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_photographer)
+    }
+}
