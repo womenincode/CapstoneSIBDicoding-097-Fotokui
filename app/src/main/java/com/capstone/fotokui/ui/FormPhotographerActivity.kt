d142edbe124f36ce5d0d161a9065b9e1bb3daed7
rendiputra <rendiputrapradana@gmail.com>
2022-12-07 16:30:55 +0700

    Add FormPhotographer features

---
+++ app/src/main/java/com/capstone/fotokui/ui/FormPhotographerActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.fotokui.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.fotokui.R
+
+class FormPhotographerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_photographer)
+    }
+}
